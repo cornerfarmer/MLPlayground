30857613fc1f3d2d4bdf9862531f6a398371bbb7
domin1101 <dominik.winkelbauer@outlook.de>
2017-07-05 23:03:37 +0200

    Adds multiple improvements

--- CPPN-GAN/train.py
+++ CPPN-GAN/train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.contrib import learn
+from tensorflow.contrib.learn.python.learn.datasets.mnist import DataSet
 from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,21 +10,75 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 
+rand_input_size = 1
+generator_steps_per_iteration = 5
+batch_size = 500
+iterations = 100000
+
 class CPPN:
     def __init__(self, sess):
         with tf.variable_scope("generator"):
-            self.x = tf.placeholder(tf.float32, (None, 784, 11))
+            self.x = tf.placeholder(tf.float32, (None, 784, rand_input_size + 3))
 
             layer_input = self.x
             for i in range(4):
-                layer_input = tf.layers.dense(layer_input, 32, tf.nn.tanh)
+                layer_input = tf.layers.dense(layer_input, 128, tf.nn.tanh, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer)
 
-            self.y = tf.layers.dense(layer_input, 1, tf.nn.sigmoid)
+            self.y = tf.layers.dense(layer_input, 1, tf.nn.sigmoid, kernel_initializer=tf.random_normal_initializer, bias_initializer=tf.random_normal_initializer)
+
+            #self.y = tf.slice(self.x, [0, 0, 1], [-1, -1, 1]) / 10
 
             self.sess = sess
 
     def predict(self, x):
         return self.sess.run(self.y, {self.x: x})
+
+    def create_input(self, x_vec, y_vec):
+        z = np.random.uniform(-1.0, 1.0, size=(1, rand_input_size)).repeat(len(y_vec) * len(x_vec), 0)
+
+        input = np.transpose([np.tile(x_vec, len(y_vec)), np.repeat(y_vec, len(x_vec))])
+
+        i_t = input.transpose()
+        r = np.sqrt(i_t[0] * i_t[0] + i_t[1] * i_t[1]).reshape((len(i_t[0]), 1))
+
+        return np.concatenate((input, r, z), axis=1)
+
+    def create_batch_input(self, x_vec, y_vec, batch_size):
+        batch = np.ndarray((batch_size, len(y_vec) * len(x_vec), rand_input_size + 3))
+        for i in range(batch_size):
+            batch[i] = self.create_input(x_vec, y_vec)
+        return batch
+
+class MLPGenerator:
+    def __init__(self, sess):
+        with tf.variable_scope("generator"):
+            self.x = tf.placeholder(tf.float32, (None, rand_input_size))
+
+            layer_input = self.x
+            layer_input = tf.layers.dense(layer_input, 128, tf.nn.relu)
+
+            self.y = tf.layers.dense(layer_input, 784, tf.nn.sigmoid)
+
+            self.sess = sess
+
+    def predict(self, x):
+        return self.sess.run(self.y, {self.x: x})
+
+    def create_batch_input(self, x_vec, y_vec, batch_size):
+       return np.random.uniform(-1.0, 1.0, size=(batch_size, rand_input_size))
+
+class MLPDiscriminator:
+    def __init__(self, sess, x, reuse):
+        with tf.variable_scope("discriminator"):
+            if reuse:
+                tf.get_variable_scope().reuse_variables()
+
+            layer_input = tf.reshape(x, (-1, 28 * 28))
+            layer_input = tf.layers.dense(layer_input, 128, tf.nn.relu)
+
+            self.y = tf.layers.dense(layer_input, 1)
+
+            self.sess = sess
 
 
 class Discriminator:
@@ -32,13 +87,15 @@ class Discriminator:
             if reuse:
                 tf.get_variable_scope().reuse_variables()
 
-            conv1 = tf.layers.conv2d(x, 32, (5, 5), padding="same", activation=tf.nn.relu)
+            conv1 = tf.layers.conv2d(x, 24, (5, 5), padding="same", activation=tf.nn.relu)
             pool1 = tf.layers.max_pooling2d(conv1, [2, 2], 2)
-            conv2 = tf.layers.conv2d(pool1, 64, [5, 5], padding="same", activation=tf.nn.relu)
+            conv2 = tf.layers.conv2d(pool1, 24 * 2, [5, 5], padding="same", activation=tf.nn.relu)
             pool2 = tf.layers.max_pooling2d(conv2, [2, 2], 2)
+            conv3 = tf.layers.conv2d(pool2, 24 * 4, [5, 5], padding="same", activation=tf.nn.relu)
+            pool3 = tf.layers.max_pooling2d(conv3, [2, 2], 2)
 
-            pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-            self.y = tf.layers.dense(pool2_flat, 1, None)
+            pool3_flat = tf.reshape(pool3, [-1, 3 * 3 * 24 * 4])
+            self.y = tf.layers.dense(pool3_flat, 1)
 
             self.sess = sess
 
@@ -58,18 +115,20 @@ class Models:
         self.generator = CPPN(self.sess)
         x = tf.reshape(self.generator.y, [-1, 28, 28, 1])
         tf.summary.image("generator", x, 1)
-        self.discriminator_fake = Discriminator(self.sess, x, False)
+        self.discriminator_fake = MLPDiscriminator(self.sess, x, False)
 
         self.generator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.true_labels, logits=self.discriminator_fake.y))
         self.discriminator_loss_false = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.false_labels, logits=self.discriminator_fake.y))
 
         vars = [var for var in tf.trainable_variables() if "generator/" in var.name]
-        self.train_step_gen = tf.train.AdamOptimizer(0.005, name="AdamGenerator").minimize(self.generator_loss, var_list=vars)
+        self.gen_optimizer = tf.train.AdamOptimizer(0.005, name="AdamGenerator")
+        self.train_step_gen = self.gen_optimizer.minimize(self.generator_loss, var_list=vars)
 
     def _build_discriminator(self):
         self.d_input = tf.placeholder(tf.float32, (None, 28 * 28))
         x = tf.reshape(self.d_input, [-1, 28, 28, 1])
-        self.discriminator_orig = Discriminator(self.sess, x, True)
+        tf.summary.image("discriminator", x, 1)
+        self.discriminator_orig = MLPDiscriminator(self.sess, x, True)
 
         self.discriminator_loss_true = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.true_labels, logits=self.discriminator_orig.y))
         self.discriminator_loss = self.discriminator_loss_false + self.discriminator_loss_true
@@ -77,22 +136,8 @@ class Models:
 
         vars = [var for var in tf.trainable_variables() if "discriminator/" in var.name]
         self.train_step_dis = tf.cond(tf.logical_and(self.generator_loss < 0.8, self.discriminator_loss > 0.45), lambda: tf.train.AdamOptimizer(0.001, name="AdamDiscriminator").minimize(self.discriminator_loss, var_list=vars), lambda: tf.constant(False))
+        #self.train_step_dis = tf.train.AdamOptimizer(0.001, name="AdamDiscriminator").minimize(self.discriminator_loss, var_list=vars)
 
-    def _create_generator_input(self, x_vec, y_vec):
-        z = np.random.uniform(-1.0, 1.0, size=(1, 8)).repeat(len(y_vec) * len(x_vec), 0)
-
-        input = np.transpose([np.tile(x_vec, len(y_vec)), np.repeat(y_vec, len(x_vec))])
-
-        i_t = input.transpose()
-        r = np.sqrt(i_t[0] * i_t[0] + i_t[1] * i_t[1]).reshape((len(i_t[0]), 1))
-
-        return np.concatenate((input, r, z), axis=1)
-
-    def _create_generator_batch_input(self, x_vec, y_vec, batch_size):
-        batch = np.ndarray((batch_size, len(y_vec) * len(x_vec), 11))
-        for i in range(batch_size):
-            batch[i] = self._create_generator_input(x_vec, y_vec)
-        return batch
 
 
     def train(self, x_vec, y_vec):
@@ -101,19 +146,36 @@ class Models:
         tf.summary.scalar("discriminator_loss_true", self.discriminator_loss_true)
         tf.summary.scalar("discriminator_loss_false", self.discriminator_loss_false)
         tf.summary.scalar("generator_loss", self.generator_loss)
+        tf.summary.scalar("generator_learning_rate", self.gen_optimizer._lr_t)
 
         merged_summary_op = tf.summary.merge_all()
 
-        for i in range(10000):
-            batch = mnist.train.next_batch(500)
-            _, _, summary = self.sess.run([self.train_step_gen, self.train_step_dis, merged_summary_op], feed_dict={self.d_input: batch[0], self.generator.x: self._create_generator_batch_input(x_vec, y_vec, 500)})
+        images = np.ndarray((1, 784))
+        labels = np.ndarray((1, 10))
+        new_i = 0
+        for i in range(len(mnist.train.labels)):
+            if mnist.train.labels[i][0] > 0:
+                images[new_i] = mnist.train.images[i] * 255
+                labels[new_i] = mnist.train.labels[i]
+                new_i += 1
+                break
 
-            train_writer.add_summary(summary, i)
+        mnist_ones = DataSet(images, labels, reshape=False)
+       # mnist.train.images = images
+       # mnist.train.labels = labels
 
-            print("Finished step " + str(i))
+        for i in range(iterations * generator_steps_per_iteration):
+            if i % generator_steps_per_iteration == 0:
+                batch = mnist_ones.next_batch(batch_size)
+                _, _, summary = self.sess.run([self.train_step_gen, self.train_step_dis, merged_summary_op], feed_dict={self.d_input: batch[0], self.generator.x: self.generator.create_batch_input(x_vec, y_vec, batch_size)})
+                train_writer.add_summary(summary, i / generator_steps_per_iteration)
+                print("Finished step " + str(i / generator_steps_per_iteration))
+            else:
+                _ = self.sess.run([self.train_step_gen], feed_dict={self.generator.x: self.generator.create_batch_input(x_vec, y_vec, batch_size)})
+
 
     def generate_image(self, x_vec, y_vec, size):
-        return self.generator.predict(self._create_generator_batch_input(x_vec, y_vec, 1).astype(np.float32)).reshape((size, size))
+        return self.generator.predict(self.generator.create_batch_input(x_vec, y_vec, 1).astype(np.float32)).reshape((size, size))
 
 class Main:
     def __init__(self):
